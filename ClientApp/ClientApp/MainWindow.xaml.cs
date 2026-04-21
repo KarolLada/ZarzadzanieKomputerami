9cdc066174c2877fc574c72013a4533fc1e1a7ac
Banaszek_Kamil <146319257+KamilBanaszek@users.noreply.github.com>
2026-04-21 11:42:05 +0200

    Poprawa logiki aplikacji
    Refactored MainWindow to use DispatcherTimer for UI updates and added mouse hook functionality.

--- ClientApp/ClientApp/MainWindow.xaml.cs
+++ ClientApp/ClientApp/MainWindow.xaml.cs
@@ -1,159 +1,274 @@
-
 using System;
+
 using System.Diagnostics;
+
 using System.Net.Sockets;
+
 using System.Runtime.InteropServices;
+
 using System.Text;
+
 using System.Threading;
-using System.Timers;
+
 using System.Windows;
 
+using System.Windows.Threading;
+ 
 namespace ClientApp
-{
-    public partial class MainWindow : Window
-    {
-        TcpClient client;
 
+{
+
+    public partial class MainWindow : Window
+
+    {
+
+        TcpClient client;
+ 
         bool isLocked = false;
 
         int remainingTime = 0;
-        System.Timers.Timer lockTimer;
-        System.Timers.Timer uiTimer;
+ 
+        DispatcherTimer timer;
+ 
+        // ===== KEYBOARD HOOK =====
 
-        // keyboard hook
-        private static IntPtr hookID = IntPtr.Zero;
+        private static IntPtr keyboardHookID = IntPtr.Zero;
+
         private delegate IntPtr LowLevelProc(int nCode, IntPtr wParam, IntPtr lParam);
-        private LowLevelProc proc;
 
+        private LowLevelProc keyboardProc;
+ 
         private const int WH_KEYBOARD_LL = 13;
+ 
+        // ===== MOUSE HOOK =====
 
+        private static IntPtr mouseHookID = IntPtr.Zero;
+
+        private LowLevelProc mouseProc;
+ 
+        private const int WH_MOUSE_LL = 14;
+ 
         [DllImport("user32.dll")]
+
         private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelProc lpfn, IntPtr hMod, uint dwThreadId);
-
+ 
         [DllImport("user32.dll")]
+
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
-
+ 
         [DllImport("user32.dll")]
+
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
+ 
         [DllImport("kernel32.dll")]
+
         private static extern IntPtr GetModuleHandle(string lpModuleName);
-
+ 
         public MainWindow()
+
         {
+
             InitializeComponent();
+
             Connect();
+ 
+            // keyboard
 
-            proc = HookCallback;
-            hookID = SetHook(proc);
+            keyboardProc = KeyboardCallback;
+
+            keyboardHookID = SetHook(keyboardProc, WH_KEYBOARD_LL);
+ 
+            // mouse
+
+            mouseProc = MouseCallback;
+
+            mouseHookID = SetHook(mouseProc, WH_MOUSE_LL);
+ 
+            // timer
+
+            timer = new DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+
+            timer.Tick += Timer_Tick;
+
         }
-
+ 
         private void Connect()
+
         {
+
             client = new TcpClient("127.0.0.1", 5000);
-
+ 
             Thread t = new Thread(Listen);
+
             t.IsBackground = true;
+
             t.Start();
-        }
 
+        }
+ 
         private void Listen()
+
         {
+
             var stream = client.GetStream();
+
             byte[] buffer = new byte[1024];
-
+ 
             while (true)
+
             {
+
                 int bytes = stream.Read(buffer, 0, buffer.Length);
+
                 string msg = Encoding.UTF8.GetString(buffer, 0, bytes);
-
+ 
                 Dispatcher.Invoke(() =>
-                {
-                    if (msg == "BLOCK")
-                        Lock();
-                    else if (msg == "UNBLOCK")
-                        Unlock();
-                    else if (msg.StartsWith("TIMER:"))
-                        StartTimer(int.Parse(msg.Split(':')[1]));
-                });
-            }
-        }
 
+                {
+
+                    if (msg == "BLOCK")
+
+                        Lock();
+
+                    else if (msg == "UNBLOCK")
+
+                        Unlock();
+
+                    else if (msg.StartsWith("TIMER:"))
+
+                        StartTimer(int.Parse(msg.Split(':')[1]));
+
+                });
+
+            }
+
+        }
+ 
         private void StartTimer(int seconds)
+
         {
-            lockTimer?.Stop();
-            uiTimer?.Stop();
 
             remainingTime = seconds;
 
-            lockTimer = new System.Timers.Timer(1000);
-            lockTimer.Elapsed += (s, e) =>
-            {
-                remainingTime--;
+            TimerText.Text = $"Pozostały czas: {remainingTime}s";
 
-                if (remainingTime <= 0)
-                {
-                    lockTimer.Stop();
-                    Dispatcher.Invoke(Lock);
-                }
-            };
-            lockTimer.Start();
+            timer.Start();
 
-            uiTimer = new System.Timers.Timer(1000);
-            uiTimer.Elapsed += (s, e) =>
-            {
-                Dispatcher.Invoke(() =>
-                {
-                    TimerText.Text = $"Pozostały czas: {remainingTime}s";
-                });
-            };
-            uiTimer.Start();
         }
+ 
+        private void Timer_Tick(object sender, EventArgs e)
 
+        {
+
+            remainingTime = Math.Max(0, remainingTime - 1);
+ 
+            if (remainingTime > 0)
+
+                TimerText.Text = $"Pozostały czas: {remainingTime}s";
+
+            else
+
+                TimerText.Text = "";
+ 
+            if (remainingTime <= 0)
+
+            {
+
+                timer.Stop();
+
+                Lock();
+
+            }
+
+        }
+ 
         private void Lock()
+
         {
+
             isLocked = true;
+
             BlockOverlay.Visibility = Visibility.Visible;
-        }
 
+        }
+ 
         private void Unlock()
+
         {
+
             isLocked = false;
+
             BlockOverlay.Visibility = Visibility.Collapsed;
-
+ 
             remainingTime = 0;
+
             TimerText.Text = "";
+ 
+            timer.Stop();
 
-            lockTimer?.Stop();
-            uiTimer?.Stop();
         }
-
+ 
         // ===== KEYBOARD BLOCK =====
 
-        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        private IntPtr KeyboardCallback(int nCode, IntPtr wParam, IntPtr lParam)
+
         {
+
             if (isLocked)
+
                 return (IntPtr)1;
+ 
+            return CallNextHookEx(keyboardHookID, nCode, wParam, lParam);
 
-            return CallNextHookEx(hookID, nCode, wParam, lParam);
         }
+ 
+        // ===== MOUSE BLOCK =====
 
-        private IntPtr SetHook(LowLevelProc proc)
+        private IntPtr MouseCallback(int nCode, IntPtr wParam, IntPtr lParam)
+
         {
+
+            if (isLocked)
+
+                return (IntPtr)1;
+ 
+            return CallNextHookEx(mouseHookID, nCode, wParam, lParam);
+
+        }
+ 
+        private IntPtr SetHook(LowLevelProc proc, int hookType)
+
+        {
+
             using (Process curProcess = Process.GetCurrentProcess())
+
             using (ProcessModule curModule = curProcess.MainModule)
+
             {
-                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+
+                return SetWindowsHookEx(hookType, proc,
+
                     GetModuleHandle(curModule.ModuleName), 0);
+
             }
-        }
 
+        }
+ 
         protected override void OnClosed(EventArgs e)
-        {
-            UnhookWindowsHookEx(hookID);
-            base.OnClosed(e);
-        }
-    }
-}
 
-//
+        {
+
+            UnhookWindowsHookEx(keyboardHookID);
+
+            UnhookWindowsHookEx(mouseHookID);
+
+            base.OnClosed(e);
+
+        }
+
+    }
+
+}
+ 
